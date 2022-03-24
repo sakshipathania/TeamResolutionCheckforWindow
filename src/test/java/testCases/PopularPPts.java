2e39e55be3ef4ea280688c181b8417965d9e6e98
Nisha Dhiman <91591950+nishaslidetech@users.noreply.github.com>
2022-03-24 10:47:15 +0530

    Update PopularPPts.java

--- src/test/java/testCases/PopularPPts.java
+++ src/test/java/testCases/PopularPPts.java
@@ -18,7 +18,7 @@ public class PopularPPts extends BaseClass {
 		};
 	}
 
-	@Test(dataProvider = "windowResolution", enabled = false)
+	@Test(dataProvider = "windowResolution", enabled = true)
 	public void checkResolutionForNewlyProducts(int w, int h) throws InterruptedException {
 		setDriver(w, h);
 		System.out.println("Resolution = " + w + "*"+ h );
